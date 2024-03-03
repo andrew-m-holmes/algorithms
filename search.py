da9975542991f5912df6d40177e794760d70e4fb
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-03 13:07:28 -0500

    keep connectivity

---
+++ search.py
@@ -0,0 +1,40 @@
+from typing import Dict, List, Optional
+
+
+def last_visited(graph: Dict[str, List[str]]) -> Optional[str]:
+    last = None
+
+    def dfs(node: str, visit: set):
+        nonlocal last
+        last = node
+        for neighbor in graph[node]:
+            if neighbor in visit:
+                continue
+            visit.add(neighbor)
+            dfs(neighbor, visit)
+
+    visit = set()
+    for node in graph.keys():
+        if node not in visit:
+            visit.add(node)
+            dfs(node, visit)
+    return last
+
+
+def main():
+    graph = {
+        "A": ["B", "C"],
+        "B": ["A", "D", "E"],
+        "C": ["A", "F"],
+        "D": ["B"],
+        "E": ["B", "G"],
+        "F": ["C"],
+        "G": ["E"],
+    }
+
+    last_node = last_visited(graph)
+    print(last_node)
+
+
+if __name__ == "__main__":
+    main()
